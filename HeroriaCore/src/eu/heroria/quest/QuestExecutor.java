1a3028abe06b52a9342aed86b7af43942b39d044
Cyriaque Perier <34946445+alph4ice@users.noreply.github.com>
2020-03-08 09:36:29 +0100

    Delete QuestExecutor.java

--- HeroriaCore/src/eu/heroria/quest/QuestExecutor.java
+++
@@ -1,9 +0,0 @@
-package eu.heroria.quest;
-
-public interface QuestExecutor {
-	public void onStart();
-	public void onFinish();
-	public void onEnable();
-	//public void onLoot(Player event);
-	public void onDead();
-}
